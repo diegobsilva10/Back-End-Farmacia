2963a801295dd4256da7ca8b0e0126ee264ff648
diegobsailva10 <diegobdasilva12@gmail.com>
2021-09-03 14:36:10 -0300

    Getter and Setters, Constructor Implemented

--- farmacia/src/main/java/org/generation/farmacia/models/Categoria.java
+++ farmacia/src/main/java/org/generation/farmacia/models/Categoria.java
@@ -1,4 +1,61 @@
 package org.generation.farmacia.models;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+/**
+ * Classe utilizada como entidade no banco de dados para a criação das categorias.
+ *
+ * @author Diego Silva
+ * @since 1.0
+ *
+ */
+
+
+@Entity
+@Table(name="produtos")
 public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long idCategoria;
+
+    @NotBlank(message = "Descrição da Categoria")
+    @Size (min = 5, max = 100)
+    private String decricacao;
+
+    @Temporal(TemporalType.TIMESTAMP)
+
+    private Date date = new java.sql.Date(System.currentTimeMillis());
+
+    public Categoria(long idCategoria, String decricacao, Date date) {
+        this.idCategoria = idCategoria;
+        this.decricacao = decricacao;
+        this.date = date;
+    }
+
+    public long getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setId(long id) {
+        this.idCategoria = id;
+    }
+
+    public String getDecricacao() {
+        return decricacao;
+    }
+
+    public void setDecricacao(String decricacao) {
+        this.decricacao = decricacao;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
--- farmacia/src/main/java/org/generation/farmacia/models/Produto.java
+++ farmacia/src/main/java/org/generation/farmacia/models/Produto.java
@@ -1,4 +1,65 @@
 package org.generation.farmacia.models;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "produto")
 public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long idProduto;
+
+    @NotBlank(message = "Nome do produto")
+    @Size (min = 10, max = 200)
+    private String nomeProduto;
+
+    @NotBlank(message = "Insira a descrição do produto")
+    @Size (min=10, max = 200)
+    private String descricaoProduto;
+
+    @NotBlank (message = "Insira o preço do produto")
+    private float preco;
+
+    public Produto(long idProduto, String nomeProduto, String descricaoProduto, float preco) {
+        this.idProduto = idProduto;
+        this.nomeProduto = nomeProduto;
+        this.descricaoProduto = descricaoProduto;
+        this.preco = preco;
+    }
+
+    public long getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(long idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public String getDescricaoProduto() {
+        return descricaoProduto;
+    }
+
+    public void setDescricaoProduto(String descricaoProduto) {
+        this.descricaoProduto = descricaoProduto;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
 }
