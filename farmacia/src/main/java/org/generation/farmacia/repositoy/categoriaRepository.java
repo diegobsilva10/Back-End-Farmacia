6e6c7e6d36021ade6ececa47ba733978a923cca1
diegobsailva10 <diegobdasilva12@gmail.com>
2021-09-03 15:49:31 -0300

    repository conectado com o BD

---
+++ farmacia/src/main/java/org/generation/farmacia/repositoy/categoriaRepository.java
@@ -0,0 +1,13 @@
+package org.generation.farmacia.repositoy;
+
+import org.generation.farmacia.models.CategoriaModels;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface categoriaRepository extends JpaRepository<CategoriaModels, Long>{
+    public List<CategoriaModels> findAllByDescricaoContainingIgnoreCase(String descricao);
+
+}
