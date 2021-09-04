8825646aff7d405464020d6d0f4c183e233f5cf4
diegobsailva10 <diegobdasilva12@gmail.com>
2021-09-04 01:57:16 -0300

    GET PUT POST DELETE implementado

--- farmacia/src/main/java/org/generation/farmacia/controller/produtoController.java
+++ farmacia/src/main/java/org/generation/farmacia/controller/produtoController.java
@@ -1,4 +1,54 @@
 package org.generation.farmacia.controller;
 
+
+import org.generation.farmacia.models.ProdutoModels;
+import org.generation.farmacia.repositoy.produtoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/produtos")
+@CrossOrigin("*")
 public class produtoController {
+
+    @Autowired
+    private produtoRepository repository;
+
+    @GetMapping
+    public ResponseEntity<List<ProdutoModels>> GetAll()   {
+        return ResponseEntity.ok(repository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProdutoModels> GetById(@PathVariable long idProduto){
+        return repository.findById(idProduto).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound()
+                .build());
+    }
+    @GetMapping("/nomeProduto/{nomeProduto}")
+    public ResponseEntity<List<ProdutoModels>> GetBynomeProduto (@PathVariable String nomeProduto){
+        return ResponseEntity.ok(repository.findAllByNomeProdutoContainingIgnoreCase(nomeProduto));
+    }
+
+    @PostMapping
+    public ResponseEntity<ProdutoModels> post (@RequestBody ProdutoModels produtoModels) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(produtoModels));
+    }
+    @PutMapping
+    public ResponseEntity<ProdutoModels> put (@RequestBody ProdutoModels produtoModels) {
+        return ResponseEntity.status(HttpStatus.OK).body(repository.save(produtoModels));
+    }
+    @DeleteMapping ("/{id}")
+    public void delete (@PathVariable long id){
+        repository.deleteById(id);
+    }
+
+
+
+
+
+
 }
