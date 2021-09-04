e711f7a39b888072513305430b6bc57bf17e9edb
diegobsailva10 <diegobdasilva12@gmail.com>
2021-09-04 01:24:10 -0300

    POST PUT DELETE implementado

--- farmacia/src/main/java/org/generation/farmacia/controller/categoriaController.java
+++ farmacia/src/main/java/org/generation/farmacia/controller/categoriaController.java
@@ -4,6 +4,7 @@ package org.generation.farmacia.controller;
 import org.generation.farmacia.models.CategoriaModels;
 import org.generation.farmacia.repositoy.categoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,6 +28,26 @@ public class categoriaController {
         return repository.findById(idCategoria).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound()
                 .build());
     }
+    @GetMapping("/nomeCategoria/{nomeCategoria}")
+    public ResponseEntity<List<CategoriaModels>> GetBynomeCategoria(@PathVariable String nomeCategoria){
+        return ResponseEntity.ok(repository.findAllByNomeCategoriaContainingIgnoreCase(nomeCategoria));
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoriaModels> post (@RequestBody CategoriaModels categoriaModels) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(categoriaModels));
+    }
+    @PutMapping
+    public ResponseEntity<CategoriaModels> put (@RequestBody CategoriaModels categoriaModels) {
+        return ResponseEntity.status(HttpStatus.OK).body(repository.save(categoriaModels));
+    }
+    @DeleteMapping ("/{id}")
+    public void delete (@PathVariable long id){
+        repository.deleteById(id);
+    }
+
+
+
 
 
 
