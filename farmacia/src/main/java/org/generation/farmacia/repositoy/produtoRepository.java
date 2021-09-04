b1fc0bbe7ada0a412329a756bace364519fd5771
diegobsailva10 <diegobdasilva12@gmail.com>
2021-09-04 01:56:30 -0300

    Repository implementado

---
+++ farmacia/src/main/java/org/generation/farmacia/repositoy/produtoRepository.java
@@ -0,0 +1,14 @@
+package org.generation.farmacia.repositoy;
+
+
+import org.generation.farmacia.models.ProdutoModels;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface produtoRepository extends JpaRepository<ProdutoModels, Long> {
+    public List<ProdutoModels> findAllByNomeProdutoContainingIgnoreCase(String nomeProduto);
+
+}
